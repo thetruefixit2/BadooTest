3d0f8f42ae0e33792ab0f0f527c6e89b151f841e
Daniil Belevtsev(HOME) <thetruefixit@gmail.com>
2017-02-03 20:40:23 +0300

    init commit

---
+++ app/src/main/java/com/dabe/baddooooo/TheApp.java
@@ -0,0 +1,11 @@
+package com.dabe.baddooooo;
+
+import android.app.Application;
+
+/**
+ * Created by Daniil Belevtsev
+ * Project: Baddooooo; Skype: pandamoni1
+ */
+
+public class TheApp extends Application {
+}
